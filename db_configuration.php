ec74eb6a2b89eb222c0e11bdb1354a15d3ed07f6
tylerThrash <tjthrash2112@outlook.com>
2017-03-13 18:11:51 -0500

    Resubmitting the db_config file. When you push in the future select the option to not follow this file.

--- db_configuration.php
+++ db_configuration.php
@@ -1,22 +1,9 @@
 <?php
-	/*
-  DEFINE('DATABASE_HOST', 'localhost');
-<<<<<<< HEAD
-	DEFINE('DATABASE_DATABASE', '   ');
-	DEFINE('DATABASE_USER', 'root');
-	DEFINE('DATABASE_PASSWORD', '  '); 
 
-=======
-	DEFINE('DATABASE_DATABASE', 'name_and_synonym');
-	DEFINE('DATABASE_USER', 'prashant');
-	DEFINE('DATABASE_PASSWORD', 'password$2'); 
-	*/
-	
 	DEFINE('DATABASE_HOST', 'localhost');
 	DEFINE('DATABASE_DATABASE', 'ics325');
 	DEFINE('DATABASE_USER', 'username');
 	DEFINE('DATABASE_PASSWORD', 'password');
->>>>>>> 2e8b8ce096729deb2e7e62b134229c05e9f803e1
 	
 $db = new mysqli(DATABASE_HOST, DATABASE_USER, DATABASE_PASSWORD, DATABASE_DATABASE);
 if (mysqli_connect_errno()) {
