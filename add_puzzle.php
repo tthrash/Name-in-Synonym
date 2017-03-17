28873c4549c3ba61a2270249a7fb1e0fa7bcb4df
Jumperv3-0 <4gwebb@gmail.com>
2017-03-16 23:04:00 -0500

    Added functionality to pick a word and create a table to input its synonyms and clues

---
+++ add_puzzle.php
@@ -0,0 +1,73 @@
+<!DOCTYPE html>
+<html>
+<head>
+
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="styles/main_style.css" type="text/css">
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="styles/main_style.css" type="text/css">
+</head>
+<title>Final Project</title>
+<body>
+  <h2>Final Project</h2>
+  <h3>Team: DOLPHIN</h3>
+  <h3>Dennis Lee, Gary Webb, Prashant Shrestha, Tyler Thrash</h3>
+  <br><br><br>
+  <div class="main-container">
+  <div class="header">
+    <a href="http://puzzles.thisisjava.com/"><img class="logo" src="./pic/logo.png"></img></a>
+    <div class="imageDiv">
+	  <a href="./list_puzzles.php"><input class="headerButton" type="image" src="./pic/list.png"></a>
+	  <a href="./add_puzzle.php"><input class="headerButton" type="image" src="./pic/addPuzzle.png"></a>
+      <a href="./addWordPair.php"><input class="headerButton" type="image" src="./pic/addWord.png"></a>
+      <a href="./login.php"><input class="headerButton" type="image" src="./pic/login.png"></a>
+    </div>
+    <div class="divTitle"><font class="font">Name in Synonyms</font></div>
+    <br>
+  </div>
+	<?PHP
+		$input = "";
+		if ($_SERVER["REQUEST_METHOD"] == "POST") {
+			if (isset($_POST["puzzleWord"])) {
+				$input = validate_input($_POST["puzzleWord"]);
+				echo create_puzzle(strlen($input), $input);
+			}
+		}
+		else {
+			echo '<form action="add_puzzle.php" method="post">
+			<div class="inputDiv"><input type="textbox" name="puzzleWord" id="name-textbox" placeholder="Enter a name to create a puzzle"></input>
+			</div>
+			<br><input class="main-buttons align" type="submit" name="submit" value="Show me.."></form>';
+		}
+		
+		function validate_input($data) {
+			$data = trim($data);
+			$data = stripslashes($data);
+			$data = htmlspecialchars($data);
+			$data = preg_replace('/\s+/', '', $data);
+			return $data;
+		}
+		
+		function create_puzzle($size, $word) {
+			$table = "";
+			$table .= "<h1>Enter the words and clues for <div class='red'>" . $word . "</div></h1>";
+			$table .= "<table><thead><tr><th>No</th><th>Character</th><th>Synonym (word)</th><th>Clue</th></thead>";
+			for ($i = 0; $i < $size; $i++) {
+				if ($i == 0) {
+					$table .= "<tbody>";
+				}
+				$char = substr($word, $i, 1);
+				$table .= "<tr><td>" . ($i + 1) . "</td><td>" . strtoupper($char) . "</td><td><input type='text'/></td><td><input type='text'/></td></tr>";
+			}
+			$table .= "</tbody></table>";
+			return $table;
+		}
+	?>
+  
+  </div>
+</body>
+</html>
