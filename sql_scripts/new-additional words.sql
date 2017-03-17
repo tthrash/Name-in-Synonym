85e30d2722c778f6aea86d9798e6dcd123da77ec
dennislee00 <jr7854ht@metrostate.edu>
2017-03-17 11:51:31 -0500

    Add files via upload

---
+++ sql_scripts/new-additional words.sql
@@ -0,0 +1,17 @@
+use ics325;
+
+INSERT INTO word (word_id, word, rep_id) VALUES
+(13, 'mat', 1),
+(14, 'mad', 1),
+(15, 'took', 3),
+(16, 'run', 3),
+(17, 'action', 5),
+(18, 'bold', 5),
+(19, 'arrow', 7),
+(20, 'ocean', 7),
+(21, 'come', 9),
+(22, 'bargin', 9),
+(23, 'numb', 11),
+(24, 'aim', 11)
+(22, 'bold', 9),
+(23, 'no', 11);
