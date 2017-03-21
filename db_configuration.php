583b4386db195a8d5759d4735d97b08facb37b02
Jumperv3-0 <4gwebb@gmail.com>
2017-03-21 16:09:34 -0500

    added to project sheet

--- db_configuration.php
+++ db_configuration.php
@@ -1,10 +1,8 @@
 <?php
 	DEFINE('DATABASE_HOST', 'localhost');
 	DEFINE('DATABASE_DATABASE', 'ics325');
-	//DEFINE('DATABASE_USER', 'root');
-	//DEFINE('DATABASE_PASSWORD', 'ics3117');
-	DEFINE('DATABASE_USER', 'username');
-	DEFINE('DATABASE_PASSWORD', 'password');
+	DEFINE('DATABASE_USER', 'root');
+	DEFINE('DATABASE_PASSWORD', 'ics3117');
 	
 $db = new mysqli(DATABASE_HOST, DATABASE_USER, DATABASE_PASSWORD, DATABASE_DATABASE);
 if (mysqli_connect_errno()) {
