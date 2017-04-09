25cfd3ce915b71465c488d2e53473c7025905c17
tylerThrash <tjthrash2112@outlook.com>
2017-04-09 18:09:54 -0500

    Minor stuff.

--- db_configuration.php
+++ db_configuration.php
@@ -6,30 +6,30 @@
 	
 	function run_sql($sql_script)
 	{
-			$db = new mysqli(DATABASE_HOST, DATABASE_USER, DATABASE_PASSWORD, DATABASE_DATABASE);
-			// check connection
-			if ($db->connect_error)
+		$db = new mysqli(DATABASE_HOST, DATABASE_USER, DATABASE_PASSWORD, DATABASE_DATABASE);
+		// check connection
+		if ($db->connect_error)
+		{
+		trigger_error('Database connection failed: '  . $db->connect_error, E_USER_ERROR);
+		$db->close;
+		return false;
+		}
+		else
+		{
+			$result = $db->query($sql_script);
+			if(!$result)
 			{
-			  trigger_error('Database connection failed: '  . $db->connect_error, E_USER_ERROR);
-			  $db->close;
-			  return false;
+				trigger_error('Invalid SQL: ' . $sql_script . '; Error: ' . $db->error, E_USER_ERROR);
+				$result->close;
+				$db->close;
+				return false;
 			}
 			else
 			{
-				$result = $db->query($sql_script);
-				if(!$result)
-				{
-					trigger_error('Invalid SQL: ' . $sql_script . '; Error: ' . $db->error, E_USER_ERROR);
-					$result->close;
-					$db->close;
-					return false;
-				}
-				else
-				{
-					$result->close;
-					$db->close;
-					return $result;
-				}
+				$result->close;
+				$db->close;
+				return $result;
 			}
+		}
 	}
 ?>
