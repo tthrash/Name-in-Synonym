9fc5356a8c019a69f3c1a799f42dee8a062149bb
tylerThrash <tjthrash2112@outlook.com>
2017-04-09 18:10:58 -0500

    Minor.

--- db_configuration.php
+++ db_configuration.php
@@ -10,9 +10,9 @@
 		// check connection
 		if ($db->connect_error)
 		{
-		trigger_error('Database connection failed: '  . $db->connect_error, E_USER_ERROR);
-		$db->close;
-		return false;
+			trigger_error('Database connection failed: '  . $db->connect_error, E_USER_ERROR);
+			$db->close;
+			return false;
 		}
 		else
 		{
