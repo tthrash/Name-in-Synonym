2a8b378974d97a7fa22ccc68af41d4e3bc2a57eb
DESKTOP-4I3S8IS\metal <psshrestha111@gmail.com>
2017-04-09 15:49:46 -0500

    tried changing admin page. CSS not responsive

--- admin.php
+++ admin.php
@@ -25,16 +25,17 @@
 		<div id="export">
 			<a href="admin_edit_synonyms.php">[1]Edit Synonyms for the word</a>
 			<a href="export_db.php">[2]Export the word list (Source: Database; Target: Excel file)</a>
+			<form method="post">
+				<a href="">[3] Import the word list (Source: Excel File; Target: Database) </a> 
+					<input type="file" name="fileToUpload" id="fileToUpload">
+				
+					<input type="submit" value="Submit File" name="submit">
+			</form>
 			<a href="export_db.php">[4] Export the logical_char list (Source: Database; Target: Excel file)</a>
 			<a href="export_db.php">[5]Export the puzzle list (Source: Database; Target: Excel file)</a>
       		<a href="admin_manage_users.php">[6] Manage Users (add, delete, update) (Extra Credit)</a>
 		</div>
-		<div id="import">
-			<form method="post" enctype="multipart/form-data">
-				<label>Select file to upload:  <input type="file" name="fileToUpload" id="fileToUpload"></label>
-				<br>
-				<input type="submit" value="Submit File" name="submit">
-			</form>
+		<div>
 		</div>
 	</div>
 </body>
