92113fab1cc72627f0d2d2de7686f2b982929032
tylerThrash <tjthrash2112@outlook.com>
2017-04-09 22:22:23 -0500

    Fixed db instance issue.

--- db_configuration.php
+++ db_configuration.php
@@ -1,27 +1,27 @@
 <?php
-  DEFINE('DATABASE_HOST', 'localhost');
-  DEFINE('DATABASE_DATABASE', 'ics325');
-  DEFINE('DATABASE_USER', 'prashant');
-  DEFINE('DATABASE_PASSWORD', 'password$2');
-	
+	DEFINE('DATABASE_HOST', 'localhost');
+	DEFINE('DATABASE_DATABASE', '   ');
+	DEFINE('DATABASE_USER', 'root');
+	DEFINE('DATABASE_PASSWORD', '   ');
+  
+	$db = new mysqli(DATABASE_HOST, DATABASE_USER, DATABASE_PASSWORD, DATABASE_DATABASE);
 	function run_sql($sql_script)
 	{
-		$db = new mysqli(DATABASE_HOST, DATABASE_USER, DATABASE_PASSWORD, DATABASE_DATABASE);
+		global $db;
 		// check connection
 		if ($db->connect_error)
 		{
-			trigger_error('Database connection failed: '  . $db->connect_error, E_USER_ERROR);
+			trigger_error(print_r(debug_backtrace()).'.Database connection failed: '  . $db->connect_error, E_USER_ERROR);
 		}
 		else
 		{
-			$result = $db->prepare($sql_script);
+			$result = $db->query($sql_script);
 			if($result === false)
 			{
-				trigger_error('Invalid SQL: ' . $sql_script . '; Error: ' . $db->error, E_USER_ERROR);
+				trigger_error('Stack Trace: '.print_r(debug_backtrace()).'Invalid SQL: ' . $sql_script . '; Error: ' . $db->error, E_USER_ERROR);
 			}
 			else
 			{
-				$result->execute();
 				return $result;
 			}
 		}
